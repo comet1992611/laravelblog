541461b4f22a35a7cd8f161ed9274fb3efe0c355
phonexay <john.noman1992611@gmail.com>
2020-08-31 11:42:02 +0700

    Update the blog module

--- vendor/webdevetc/blogetc/src/Views/blogetc_admin/layouts/admin_layout.blade.php
+++ vendor/webdevetc/blogetc/src/Views/blogetc_admin/layouts/admin_layout.blade.php
@@ -32,6 +32,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     @endif
 
+    <script type="text/javascript">
+        function expendNavs() {
+            $('#navbarSupportedContent').toggleClass("collapse");
+        }
+    </script>
 </head>
 <body>
 <div id="app">
@@ -43,7 +48,7 @@
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
+                    aria-label="{{ __('Toggle navigation') }}" onclick="expendNavs()">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
